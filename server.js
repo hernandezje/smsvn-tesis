8dea3e0ae5c627fb201c8b2c819dedaac9e8efa6
JohanaHernandez <hernandezje@gmail.com>
2024-12-17 17:38:20 -0300

    agrego los benditos ssl2

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const http = require("http");
+const fs = require('fs');
 const setupWebSocket = require("./socket");
 const Users = require("./routes/Users");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use(cors({
-  origin: 'https://babyhelp.site', // El dominio de tu frontend
-  methods: 'GET,POST,PUT,DELETE',
+const corsOptions = {
+  origin: 'https://babyhelp.site',  // El origen de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
-}));
+};
+
+app.use(cors(corsOptions));
 
 // Ruta principal para usuarios
 app.use("/users", Users);
@@ -32,7 +35,6 @@ const options = {
   key: fs.readFileSync('./server.key'),  // Ruta al archivo de clave privada
   cert: fs.readFileSync('./server.crt')  // Ruta al archivo de certificado
 };
-app.use(cors(corsOptions));
 
 const PORT = 5000;
 server.listen(PORT, () => {
